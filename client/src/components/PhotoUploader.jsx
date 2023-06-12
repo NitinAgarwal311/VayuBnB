3e7fa4d86b6497af5b5ebacaf6c7d849cff955cf
Nitin Agarwal <nitin391618@gmail.com>
2023-06-12 23:03:53 +0530

    Remove Photo Logic and Code Formatting

--- client/src/components/PhotoUploader.jsx
+++ client/src/components/PhotoUploader.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
-import axios from 'axios';
-import { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
 
-
-export default function PhotoUploader({addedPhotos, onChange}) {
+export default function PhotoUploader({ addedPhotos, onChange }) {
     const [photoLink, setPhotoLink] = useState("");
 
     const addPhotoByLink = async (ev) => {
@@ -24,66 +23,92 @@ export default function PhotoUploader({addedPhotos, onChange}) {
 
         const formData = new FormData();
 
-        for(const [,file] of Object.entries(files)) {
-            formData.append("photos",file);
+        for (const [, file] of Object.entries(files)) {
+            formData.append("photos", file);
         }
 
-        const {data} = await axios.post('/newPlace/uploadFile', formData, {
+        const { data } = await axios.post("/newPlace/uploadFile", formData, {
             headers: {
-                'Content-Type': 'multipart/form-data'
-            }
+                "Content-Type": "multipart/form-data",
+            },
         });
 
         onChange([...addedPhotos, data]);
-        
+    };
+
+    const removePhoto = (ev, photo) => {
+        ev.preventDefault();
+        onChange([...addedPhotos.filter(addedPhoto => addedPhoto !== photo)]);
     }
-  return (
-    <>
-        <div className="flex gap-2">
-                            <input
-                                type="text"
-                                placeholder="Add using a link"
-                                value={photoLink}
-                                onChange={(ev) => setPhotoLink(ev.target.value)}
+
+    return (
+        <>
+            <div className="flex gap-2">
+                <input
+                    type="text"
+                    placeholder="Add using a link"
+                    value={photoLink}
+                    onChange={(ev) => setPhotoLink(ev.target.value)}
+                />
+                <button
+                    className="bg-gray-300 px-4 rounded-2xl"
+                    onClick={addPhotoByLink}
+                >
+                    Add&nbsp;Photo
+                </button>
+            </div>
+            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                {addedPhotos.map((photo) => {
+                    return (
+                        <div key={photo} className="flex relative">
+                            <img
+                                src={`http://localhost:4000/uploads/${photo}`}
+                                alt=""
+                                className="rounded-2xl w-full object-cover"
                             />
-                            <button
-                                className="bg-gray-300 px-4 rounded-2xl"
-                                onClick={addPhotoByLink}
-                            >
-                                Add&nbsp;Photo
-                            </button>
-                        </div>
-                        <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                            {addedPhotos.map((photo) => {
-                                return (
-                                    <div key={photo} className="flex w-full object-cover">
-                                        <img
-                                            src={`http://localhost:4000/uploads/${photo}`}
-                                            alt=""
-                                            className="rounded-2xl"
-                                        />
-                                    </div>
-                                );
-                            })}
-                            <label className="border cursor-pointer bg-transparent rounded-2xl p-8 text-2xl text-gray-600 flex justify-center items-center gap-1">
-                                <input type="file" multiple className="hidden" onChange={uploadPhotoFiles}/>
+                            <button className="cursor-pointer absolute bottom-1 right-1 text-white bg-black py-2 px-4 bg-opacity-30 rounded-2xl" onClick={(ev) => {removePhoto(ev,photo)}}>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
                                     strokeWidth={1.5}
                                     stroke="currentColor"
-                                    className="w-8 h-8"
+                                    className="w-6 h-6"
                                 >
                                     <path
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
-                                        d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                                     />
                                 </svg>
-                                Upload
-                            </label>
+                            </button>
                         </div>
-    </>
-  )
+                    );
+                })}
+                <label className="border cursor-pointer bg-transparent rounded-2xl p-8 text-2xl text-gray-600 flex justify-center items-center gap-1">
+                    <input
+                        type="file"
+                        multiple
+                        className="hidden"
+                        onChange={uploadPhotoFiles}
+                    />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-8 h-8"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                        />
+                    </svg>
+                    Upload
+                </label>
+            </div>
+        </>
+    );
 }
