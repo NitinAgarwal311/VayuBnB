c0ea40f650422bc9bd699c48b03c4ae62bf6a89c
Nitin Agarwal <nitin391618@gmail.com>
2023-07-19 23:38:56 +0530

    Completing Bookings Page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import AccountNav from "../components/AccountNav";
 import axios from "axios";
+import { format, differenceInCalendarDays } from "date-fns";
 import PlaceImg from "../components/PlaceImg";
 
 export default function BookingsPage() {
@@ -18,12 +19,40 @@ export default function BookingsPage() {
             {bookings.length > 0 &&
                 bookings.map((booking) => {
                     return (
-                        <div key={booking._id}>
-                            <div>
+                        <div
+                            key={booking._id}
+                            className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden"
+                        >
+                            <div className="w-48">
                                 <PlaceImg place={booking.place} />
                             </div>
-                            <div>
-                                {booking.checkIn} -&gt; {booking.checkOut}
+                            <div className="py-3 pr-3 grow">
+                                <h2 className="text-xl">
+                                    {booking.place.title}
+                                </h2>
+                                <div className=" border-t border-gray-300 mt-2 py-2">
+                                    {format(
+                                        new Date(booking.checkIn),
+                                        "yyyy-MM-dd"
+                                    )}{" "}
+                                    &rarr;{" "}
+                                    {format(
+                                        new Date(booking.checkOut),
+                                        "yyyy-MM-dd"
+                                    )}
+                                </div>
+                                <div className="text-xl border-t border-gray-300 py-2 flex">
+                                    <div>
+                                        {differenceInCalendarDays(
+                                            new Date(booking.checkOut),
+                                            new Date(booking.checkIn)
+                                        )}{" "}
+                                        nights
+                                    </div>
+                                    <div className="ml-2 border-l border-gray-300 pl-2">
+                                        ${booking.price}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     );
